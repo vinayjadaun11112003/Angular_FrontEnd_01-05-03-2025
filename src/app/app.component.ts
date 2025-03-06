d6062f7ba11fe6afbf61dab312cfcccc1e9d8c02
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-03-06 16:10:01 +0530

    [Vinay Jadaun] - Add. Dynamic update of name based on the input provided by user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterLink],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -11,12 +12,18 @@ export class AppComponent {
   hello:string = 'Hello from BrideLabzz';
   ImageURL:string = 'https://iili.io/3F8DupV.jpg';
   redirectt:string = 'https://vinayjadaun-com.vercel.app/';
+  userName:string = '';
 
 
   // This method is called when the image is clicked
   onImageClick(event:Event):void {
     console.log("helloworld");
     window.open(this.redirectt,"_blank"); // Navigate to the new URL
+  }
+  
+
+  onInput(event:Event){
+     
   }
 
   
