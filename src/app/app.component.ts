b3603dd283de467d656c88d6dd4e5a211b0837c4
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-03-06 13:59:13 +0530

    [Vinay Jadaun] - Add. Image Redirection done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterLink,RouterOutlet],
+  imports: [RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title:string = 'Hello App';
-  hello:String = 'Hello from BrideLabzz';
-  ImageURL:String = 'https://iili.io/3F8DupV.jpg';
+  hello:string = 'Hello from BrideLabzz';
+  ImageURL:string = 'https://iili.io/3F8DupV.jpg';
+  redirectt:string = 'https://vinayjadaun-com.vercel.app/';
+
+
+  // This method is called when the image is clicked
+  onImageClick(event:Event):void {
+    console.log("helloworld");
+    window.open(this.redirectt,"_blank"); // Navigate to the new URL
+  }
+
+  
 }
